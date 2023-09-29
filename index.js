d19bee58655aa843fc94e56a4a8bc72e7a85916d
AskurTomasson <askurtomasson@gmail.com>
2023-09-29 10:33:06 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+let apiKey = 'insert API key here'
+
+/* SENDS REQUEST TO GET GENERATED BACKGROUND IMAGES */
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
+    .catch(err => {
+        // Use a default background image/author
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+		document.getElementById("author").textContent = `By: Dodi Achmad`
+    })
+
+    /* SENDS REQUEST TO GET INFORMATION ABOUT ETHEREUM */
+fetch("https://api.coingecko.com/api/v3/coins/ethereum")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+        document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `
+        document.getElementById("crypto").innerHTML += `
+            <p>🏷️: $${data.market_data.current_price.usd}</p>
+            <p>⬆️: $${data.market_data.high_24h.usd}</p>
+            <p>⬇️: $${data.market_data.low_24h.usd}</p>
+        `
+    })
+    .catch(err => console.error(err))
+
+
+/* FETCHES TIME FROM BUILT-IN JAVASCRIPT METHOD */
+function getCurrentTime() {
+    const date = new Date()
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+}
+
+setInterval(getCurrentTime, 1000)
+
+
+/* APIKEY NEEDS TO BE PURCHASED IN ORDER FOR WEATHER TO FUNCTION */
+/* COLLECTS LOCATION & SENDS REQUEST FOR WEATHER */
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&appid=${apiKey}`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round((data.main.temp - 32)) * 5/9}º</p>
+                <p class="weather-city">${data.name}</p>
+            `
+        })
+        .catch(err => console.error(err))
+});
